9f97051b0a4dd7770ce7420f537de70aaa2712ff
Akshaj Rao <117006036+cross-codes@users.noreply.github.com>
2022-12-03 11:10:43 +0530

    First fully functional version complete.

--- web_scraper.py
+++ web_scraper.py
@@ -1,4 +1,5 @@
 # web_scraper.py
+
 '''
 Link for documentation on Selenium and locating via XPath: https://selenium-python.readthedocs.io/locating-elements.html#locating-by-xpath
 '''
@@ -7,16 +8,16 @@ import os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import csv
-import prettytable
+from prettytable import PrettyTable
 
 
 driver = webdriver.Chrome(os.getcwd() + 'chromedriver')
 
 BASE_URL = "https://www.myntra.com/shoes?p="
 
-for i in range(1, 5):
-    print ("Currently on page: ", i)
-    driver.get(BASE_URL + str(i))
+for x in range(1, 5):
+    print ("Currently on page: ", x)
+    driver.get(BASE_URL + str(x))
     driver.execute_script("window.scrollTo(0,document.body.scrollHeight)") 
     # Because Myntra's page is dynamic, this ensures that all entries load by scrolling down to the bottom
 
@@ -36,12 +37,38 @@ for i in range(1, 5):
         if "sneaker" in shoe_name.lower():    
             sneaker_params[shoe_name] = rating
     
-    print ("Skipped ", skip_count, " entries due to missing information.")
-
-print (sneaker_params)
+    print ("Skipped", skip_count, "entries due to missing information.")
 
 # There is no true "category" of shoes within the pages itself, so we use a default category of "sneakers".
+file = open("sneakers.csv", "w")  # Note: "sneakers.csv" will be made in the current directory
+writer_object = csv.writer(file)
 
+column_1 = ["Sneaker_Name"] + list(sneaker_params.keys())
+column_2 = ["Category", "Sneakers"]
+column_3 = ["User_Rating (/5)"] + list(sneaker_params.values())
+
+temp_row = []
+summary_table = PrettyTable(["Sneaker_Name", "Category", "User_Rating (/5)"])
+
+for y in range(0, len(column_1), 1):
+
+    temp_row.append(column_1[y])
+    if y == 0:
+        temp_row.append(column_2[0])
+    else:
+        temp_row.append(column_2[1])
+    temp_row.append(column_3[y])
+
+    writer_object.writerow(temp_row)
+    if y > 0:
+        summary_table.add_row(temp_row)
+    temp_row = []
+    
+
+print ("\nThe file has been succesfully created in", os.getcwd(), "\b. A summary of the contents of the file is as shown below:\n", summary_table)
+
+file.flush()
+file.close()
 driver.close()
 
 
